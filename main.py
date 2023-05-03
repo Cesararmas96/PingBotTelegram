f581404a0c31020536ed19914a8778dc185bb59c
Cesar armas <caal2096@gmail.com>
2023-05-02 22:16:01 -0400

    funciona con una ip

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import asyncio
 import telegram
+from pythonping import ping
 
-async def send_message():
-    # Token de acceso del bot
-    TOKEN = '6021038433:AAFgNORXxoK51_xt_Rqt44SWdh3ASFF6no0'
+# Configuración de Telegram
+telegram_token = '6021038433:AAFgNORXxoK51_xt_Rqt44SWdh3ASFF6no0'
+chat_id = '318712727'
+bot = telegram.Bot(telegram_token)
 
-    # ID de chat del bot
-    CHAT_ID = '318712727'
+# Lista de direcciones IP de las computadoras que se van a verificar
+computer_ips = ['192.168.1.105']
 
-    # Crear un objeto bot con el token de acceso
-    bot = telegram.Bot(token=TOKEN)
+# Diccionario para mantener un seguimiento del estado de cada computadora
+computer_status = {ip: False for ip in computer_ips}
 
-    # Enviar un mensaje al chat especificado
-    await bot.send_message(chat_id=CHAT_ID, text='Hola, ¿cómo estás?')
 
-asyncio.run(send_message())
+async def check_computer(ip):
+    response = ping(ip, count=1)
+    if response.success:
+        if not computer_status[ip]:
+            computer_status[ip] = True
+            message = f'La computadora {ip} está conectada.'
+            await bot.send_message(chat_id, message)
+    else:
+        if computer_status[ip]:
+            computer_status[ip] = False
+            message = f'La computadora {ip} se ha desconectado.'
+            await bot.send_message(chat_id, message)
+
+
+async def main():
+    while True:
+        # Realizar ping a cada dirección IP en paralelo
+        await asyncio.gather(*[check_computer(ip) for ip in computer_ips])
+        # Esperar un tiempo antes de volver a realizar ping
+        await asyncio.sleep(60)
+
+if __name__ == '__main__':
+    asyncio.run(main())
